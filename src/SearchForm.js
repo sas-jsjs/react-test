de3d7fe5a29277819e870b88e0eb22286abe2767
Mohammed Dwina <diwina1993@hotmail.com>
2022-07-04 18:25:55 +0100

    Validate form input

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router";
+import { Alert } from "react-bootstrap";
 
+const specialChar = ["/", "%"];
 const SearchForm = (props) => {
   const [query, setQuery] = useState(null);
+  const [error, setError] = useState("");
 
   const handleInput = (event) => {
+    setError("");
     setQuery(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    props.history.push(`/search/${query}`);
+    const invalidQuery = specialChar.filter((char) => query.includes(char));
+
+    if (!invalidQuery.length) {
+      props.history.push(`/search/${query}`);
+    } else {
+      setError(
+        `Please, avoid using these characters: ${specialChar.join(", ")}`
+      );
+    }
   };
 
   return (
@@ -18,10 +30,17 @@ const SearchForm = (props) => {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          required
           className="form-control"
           placeholder="Search"
           onChange={handleInput}
         />
+        <br />
+        {error && (
+          <p style={{ color: "red" }} variant="danger">
+            {error}
+          </p>
+        )}
         <button type="submit" className="btn btn-default">
           Submit
         </button>
