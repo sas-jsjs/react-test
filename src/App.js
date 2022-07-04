17b41869c626d7f19c30e13b73899abdf9386cee
Mohammed Dwina <diwina1993@hotmail.com>
2022-07-04 18:27:01 +0100

    Wrappe the App with <Container />

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Search from "./Search.js";
-import { Bootstrap } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <h1>GitHub Search</h1>
-        <Search />
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Container>
+      <h1>GitHub Search</h1>
+      <Search />
+    </Container>
+  </Router>
+);
 
 export default App;
